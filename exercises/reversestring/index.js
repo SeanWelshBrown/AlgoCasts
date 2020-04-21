55a4db296b15cdae12d9064481203be4e0395723
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-04-21 08:31:59 -0400

    Completed Reverse String and Palindrom algos

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,34 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  let reversedStr = ""
+
+  // classic syntax
+  // for (i = 1; i <= str.length; i++) {
+  //   reversedStr += str.charAt(str.length - i)
+  // }
+
+  // ES6 syntax
+  for (const char of str) {
+    reversedStr = char + reversedStr
+  }
+
+  return reversedStr;
+}
+
+
+// alternate solution
+function reverse2(str) {
+  return str
+    .split("")
+    .reverse()
+    .join("");
+}
+
+// alternate solution2
+function reverse3(str) {
+  return str.split("").reduce((rev, char) => char + rev, "")
+}
 
 module.exports = reverse;
