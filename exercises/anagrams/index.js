e36ece80627d6b6dc84086708c16cbaaca51aeda
Sean Welsh Brown <seanwelshbrown@gmail.com>
2020-04-22 23:21:36 -0400

    Completed Anagrams algo

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,37 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  const stringAObj = {}
+  const stringBObj = {}
+  const stringAArr = []
+  const stringBArr = []
+
+  const stringANoCase = stringA.toLowerCase().replace(/[^\w]|_/g, "")
+  const stringBNoCase = stringB.toLowerCase().replace(/[^\w]|_/g, "")
+
+  for (let char of stringANoCase) {
+    stringAObj[char] = stringAObj[char] + 1 || 1;
+  }
+  for (let char of stringBNoCase) {
+    stringBObj[char] = stringBObj[char] + 1 || 1;
+  }
+
+  for (let key in stringAObj) {
+    stringAArr.push({char: key, number: stringAObj[key]})
+  }
+  for (let key in stringBObj) {
+    stringBArr.push({char: key, number: stringAObj[key]})
+  }
+
+  const stringAArrSorted = stringAArr.sort((a, b) => (a.char > b.char) ? 1 : -1)
+  const stringBArrSorted = stringBArr.sort((a, b) => (a.char > b.char) ? 1 : -1)
+
+  if (JSON.stringify(stringAArrSorted) === JSON.stringify(stringBArrSorted)) {
+    return true
+  } else {
+    return false
+  }
+}
 
 module.exports = anagrams;
