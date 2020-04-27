f4d703fabe91d861b6a79681eec1599597c997dc
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-04-27 18:15:38 -0400

    Spiral Matrix algo done

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -17,7 +17,50 @@
 
 
 function matrix(n) {
-  
+  let results = [];
+  for (i = 0; i < n; i++) {
+    results.push([])
+  };
+
+  let counter = 1;
+  let topRow = 0;
+  let bottomRow = n - 1;
+  let leftColumn = 0;
+  let rightColumn = n - 1;
+
+  while (topRow <= bottomRow && leftColumn <= rightColumn) {
+
+    // top row
+    for (i = leftColumn; i <= rightColumn; i++) {
+      results[topRow][i] = counter;
+      counter++
+    }
+    topRow++;
+
+    // right column
+    for (i = topRow; i <= bottomRow; i++) {
+      results[i][rightColumn] = counter;
+      counter++
+    }
+    rightColumn--;
+
+    // bottom row
+    for (i = rightColumn; i >= leftColumn; i--) {
+      results[bottomRow][i] = counter;
+      counter++;
+    }
+    bottomRow--;
+
+    // left column
+    for (i = bottomRow; i >= topRow; i--) {
+      results[i][leftColumn] = counter;
+      counter++;
+    }
+    leftColumn++;
+
+  }
+
+  return results;
 }
 
 
