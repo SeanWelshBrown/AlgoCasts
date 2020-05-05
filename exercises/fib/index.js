d8a180f69744d43d2769ac85aa5496a2ce17f060
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-05-05 17:24:03 -0400

    Completed Fibonacci Sequence algo

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,54 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+
+
+// iterative
+function fib(n) {
+  const nums = [0, 1];
+
+  while (nums.length <= n) {
+    nums.push(nums[nums.length - 1] + nums[nums.length - 2]);
+  }
+
+  return nums[n];
+}
+
+
+// alternate iterative
+function fib2(n) {
+  const nums = [0, 1];
+
+  for (i = 2; i <= n; i++) {
+    nums.push(nums[nums.length - 1] + nums[nums.length - 2])
+  }
+
+  return nums[n];
+}
+
+
+
+// recursive
+function fib3(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2)
+}
+
+
+// alternate recursive
+function fib4(n, nums = [0, 1]) {
+  if (nums.length > n) {
+    return nums[n];
+  }
+
+  nums.push(nums[nums.length - 1] + nums[nums.length - 2]);
+
+  return fib(n, nums);
+}
+
+
 
 module.exports = fib;
