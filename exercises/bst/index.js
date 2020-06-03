aaf5389ba5c7660a3bccfd739a9f7bd42fbd279a
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-06-03 18:36:45 -0400

    Completed Binary Search Tree insert/contains algos

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,59 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  
+  insert(data) {
+    if (data < this.data && this.left) {
+      this.left.insert(data);
+    } else if (data < this.data) {
+      this.left = new Node(data);
+    } else if (data > this.data && this.right) {
+      this.right.insert(data);
+    } else if (data > this.data) {
+      this.right = new Node(data);
+    }
+  }
+  
+  // // alternate recursive insert
+  // insert(data) {
+  //   function insertion(node) {
+  //     if (data < node.data) {
+  //       if (node.left) {
+  //         return insertion(node.left);
+  //       } else {
+  //         node.left = new Node(data);
+  //       }
+  //     } else if (data > node.data) {
+  //       if (node.right) {
+  //         return insertion(node.right);
+  //       } else {
+  //         node.right = new Node(data);
+  //       }
+  //     }
+  //   }
+  //   insertion(this);
+  // }
+
+  contains(data) {
+    if (data === this.data) {
+      return this;
+    } else if (data < this.data && this.left) {
+      return this.left.contains(data)
+    } else if (data > this.data && this.right) {
+      return this.right.contains(data) 
+    } else {
+      return null;
+    }
+  }
+}
+
 
 module.exports = Node;
