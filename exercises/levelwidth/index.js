7dacafbd3603c2a0a9ec23dddb830e941cb2cb16
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-06-03 17:11:25 -0400

    Completed Tree width algo

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,24 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+
+function levelWidth(root) {
+  const counters = [0];
+  const nodeArr = [root, "s"];
+
+  while (nodeArr.length > 1) {
+    let node = nodeArr.shift();
+
+    if (node === "s") {
+      counters.push(0);
+      nodeArr.push("s");
+    } else {
+      counters[counters.length - 1]++;
+      nodeArr.push(...node.children);
+    }
+  }
+
+  return counters;
+}
 
 module.exports = levelWidth;
