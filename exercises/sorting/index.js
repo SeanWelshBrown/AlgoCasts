939e0b62e9eca5f5a240a13a0b78c22e84b37b46
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-06-04 13:43:33 -0400

    Completed Sorting Algorithms

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,8 +1,8 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
+
 function bubbleSort(arr) {
-  // implement bubblesort
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < (arr.length - i - 1); j++) {
       if (arr[j] > arr[j + 1]) {
@@ -10,9 +10,10 @@ function bubbleSort(arr) {
       }
     }
   }
-  // return the sorted array
+
   return arr;
 }
+
 
 function selectionSort(arr) {
   for (let i = 0; i < arr.length; i++) {
@@ -30,12 +31,32 @@ function selectionSort(arr) {
   return arr;
 }
 
-function mergeSort(arr) {
 
+function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
+
+  const center = Math.floor(arr.length / 2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
+
+  return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
+  let results = [];
 
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+
+  return [...results, ...left, ...right];
 }
+
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
